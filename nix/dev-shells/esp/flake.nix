b5788387466376b97a87ae20483cd7e83c782eb5
Daniil Miskevich <daniilmiskevich@gmail.com>
2026-07-05 21:41:40 +0300

    Got my hands to the formatter, so reformats the `flake.nix`; Update to include the ncurses patch from the `nixpkgs-esp-dev-esp8266-fix`

--- nix/dev-shells/esp/flake.nix
+++ nix/dev-shells/esp/flake.nix
@@ -1,26 +1,33 @@
 {
-	description = "Preconfigured shells for ESP32 and ESP8266 development";
+  description = "Preconfigured shells for ESP32 and ESP8266 development";
 
-	inputs = {
-		nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-		esp32-pkgs.url = "github:mirrexagon/nixpkgs-esp-dev";
-		esp8266-pkgs.url = "github:DaniilAlpha/nixpkgs-esp-dev-esp8266-fix";
-	};
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    esp32-pkgs.url = "github:mirrexagon/nixpkgs-esp-dev";
+    esp8266-pkgs.url = "github:DaniilAlpha/nixpkgs-esp-dev-esp8266-fix";
+  };
 
-	outputs = { self, nixpkgs, esp32-pkgs, esp8266-pkgs }:
-		let
-			system = "x86_64-linux";
-			pkgs = nixpkgs.legacyPackages.${system};
-		in {
-			devShells.${system} = {
-				esp32 = pkgs.mkShell {
-					inputsFrom = [ esp32-pkgs.devShells.${system}.esp-idf-full ];
-					buildinputs = [ pkgs.llvmPackages.clang-tools ];
-				};
-				esp8266 = pkgs.mkShell {
-					inputsFrom = [ esp8266-pkgs.devShells.${system}.esp8266-rtos-sdk ];
-					buildinputs = [ pkgs.llvmPackages.clang-tools ];
-				};
-			};
-		};
+  outputs =
+    {
+      self,
+      nixpkgs,
+      esp32-pkgs,
+      esp8266-pkgs,
+    }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.${system} = {
+        esp32 = pkgs.mkShell {
+          inputsFrom = [ esp32-pkgs.devShells.${system}.esp-idf-full ];
+          buildinputs = [ pkgs.llvmPackages.clang-tools ];
+        };
+        esp8266 = pkgs.mkShell {
+          inputsFrom = [ esp8266-pkgs.devShells.${system}.esp8266-rtos-sdk ];
+          buildinputs = [ pkgs.llvmPackages.clang-tools ];
+        };
+      };
+    };
 }
