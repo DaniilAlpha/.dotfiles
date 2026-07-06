52e6a01313e8c98d1a000ecfb334deeb2134be1b
Daniil Miskevich <daniilmiskevich@gmail.com>
2026-07-06 11:18:52 +0300

    Adds odin dev shell

---
+++ nix/dev-shells/odin/flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "Preconfigured shell for Odin development";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.${system} = {
+        default = pkgs.mkShell {
+          buildinputs = [
+            pkgs.odin
+            pkgs.ols
+          ];
+        };
+      };
+    };
+}
